35cce4cc7989d5cb9fa78e0b6e220d93c5869744
Mayra Cabrera <mcabrera1087@gmail.com>
2016-02-04 00:13:03 -0600

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func init() {
+	orm.RegisterDataBase("default", "mysql", "root:asdfasdfa@tcp(127.0.0.1:3306)/employees")
+}
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
